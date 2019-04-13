ab42d7a520d02dcaf4f32d7fc6154ec8c4f6467b
Marian <mail@marian42.de>
2019-04-13 21:42:00 +0200

    Enable Lighting

--- meshviewer.py
+++ meshviewer.py
@@ -2,42 +2,37 @@ import pygame
 from pygame.locals import *
 from OpenGL.arrays import vbo
 from OpenGL.GL import shaders
-
+import sys
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
 import numpy as np
-    
-def printOpenGLError():
-    err = glGetError()
-    if (err != GL_NO_ERROR):
-        print('GLERROR: ', gluErrorString(err))
-        sys.exit()
 
 class Shader(object):
     def initShader(self, vertex_shader_source, fragment_shader_source):
         self.program = glCreateProgram()
-        printOpenGLError()
 
         self.vs = glCreateShader(GL_VERTEX_SHADER)
         glShaderSource(self.vs, [vertex_shader_source])
-        glCompileShader(self.vs)
         glAttachShader(self.program, self.vs)
-        printOpenGLError()
 
         self.fs = glCreateShader(GL_FRAGMENT_SHADER)
         glShaderSource(self.fs, [fragment_shader_source])
         glCompileShader(self.fs)
         glAttachShader(self.program, self.fs)
-        printOpenGLError()
 
         glLinkProgram(self.program)
-        printOpenGLError()
+
+        try:
+            glUseProgram(self.program)
+        except GLError:
+            err = glGetProgramInfoLog(self.program)
+            print(err.decode("utf-8"))
+            sys.exit()
 
     def use(self):
-        if glUseProgram(self.program):
-            printOpenGLError()
+        glUseProgram(self.program)
 
 def main():
     pygame.init()
@@ -49,27 +44,39 @@ def main():
     glClearDepth(1.0)
     glDepthFunc(GL_LESS)
     glEnable(GL_DEPTH_TEST)
+
     vertexBuffer = vbo.VBO(vertices)
-
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(display[0]) / float(display[1]), 0.1, voxel_size * 4)
-
+    normalBuffer = vbo.VBO(normals)
+    
     mouse = None
     rotation = (0, 0)
 
     shader = Shader()
     shader.initShader('''
-void main()
-{
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-}
+        varying out vec3 normal;
+
+        void main()
+        {
+            gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+            normal = (gl_ModelViewProjectionMatrix * vec4(gl_Normal, 0.0)).xyz;
+        }
     ''',
     '''
-void main()
-{
-    gl_FragColor = vec4(0.5, 0.5, 0.5, 1.0);
-}
+        in vec3 normal;
+
+        const vec3 lightDirection = vec3(0.7, 0.7, -0.14);
+        const float ambient = 0.2;
+        const float diffuse = 0.8;
+        const float specular = 0.3;
+        const vec3 viewDirection = vec3(0.0, 0.0, 1.0);
+        
+        const vec3 albedo = vec3(0.5);
+        void main() {
+            vec3 color = albedo * (ambient
+                + diffuse * (0.5 + 0.5 * dot(lightDirection, normal))
+                + specular * pow(max(0.0, dot(reflect(-lightDirection, normal), viewDirection)), 2.0));
+            gl_FragColor = vec4(color.r, color.g, color.b, 1.0);
+        }
     ''')
 
     while True:
@@ -85,28 +92,34 @@ void main()
             if pressed:
                 rotation = (rotation[0] + movement[0], max(-90, min(90, rotation[1] + movement[1])))
         
+        mouse = current_mouse
+        
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, float(display[0]) / float(display[1]), 0.1, voxel_size * 4)
+        
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
         glTranslatef(0.0, 0.0, -voxel_size * 2)
         glRotatef(rotation[1], 1.0, 0, 0)
         glRotatef(rotation[0], 0, 1.0, 0)
-        glTranslatef(-(voxel_shape[0] - 1) / 2, -(voxel_shape[0] - 1) / 2, -(voxel_shape[0] - 1) / 2)
-        
-        mouse = current_mouse
+        glTranslatef(-(voxel_shape[0] - 1) / 2, -(voxel_shape[0] - 1) / 2, -(voxel_shape[0] - 1) / 2)        
 
         glClearColor(0.01, 0.01, 0.01, 1.0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glColor4d(1.0, 1.0, 0.0, 1.0)
 
+        shader.use()
 
-        shader.use()    
         glEnableClientState(GL_VERTEX_ARRAY)
         vertexBuffer.bind()
         glVertexPointer(3, GL_FLOAT, 0, vertexBuffer)
-        glDrawArrays(GL_TRIANGLES, 0, vertices.shape[0])        
-        glUseProgram(0)
 
-        glFlush()
+        glEnableClientState(GL_NORMAL_ARRAY)
+        normalBuffer.bind()
+        glNormalPointer(GL_FLOAT, 0, normalBuffer)
+
+        glDrawArrays(GL_TRIANGLES, 0, vertices.shape[0])
 
         pygame.display.flip()
         pygame.time.wait(10)
@@ -119,8 +132,6 @@ def create_vertices(voxels_array):
     voxels[1:-1, 1:-1, 1:-1] = voxels_array
 
     mask = voxels > THRESHOLD
-    print(mask.shape)
-    print(str(np.count_nonzero(mask)) + " voxels")
 
     # X
     x, y, z = np.where(mask[:-1,:,:] & ~mask[1:,:,:])
@@ -133,9 +144,10 @@ def create_vertices(voxels_array):
         x + 1, y + 1, z + 1,
         x + 1, y, z + 1
     ]
-
     arrays = [np.array(vertices).transpose().flatten()]
+    normals = [np.tile(np.array([1, 0, 0]), 6 * x.shape[0])]
 
+    
     x, y, z = np.where(~mask[:-1,:,:] & mask[1:,:,:])
     vertices = [
         x + 1, y + 1, z,
@@ -147,9 +159,10 @@ def create_vertices(voxels_array):
         x + 1, y + 1, z
     ]
 
-    # Y
     arrays.append(np.array(vertices).transpose().flatten())
+    normals.append(np.tile(np.array([-1, 0, 0]), 6 * x.shape[0]))
 
+    # Y
     x, y, z = np.where(mask[:,:-1,:] & ~mask[:,1:,:])
     vertices = [
         x + 1, y + 1, z,
@@ -161,6 +174,7 @@ def create_vertices(voxels_array):
         x, y + 1, z + 1
     ]
     arrays.append(np.array(vertices).transpose().flatten())
+    normals.append(np.tile(np.array([0, 1, 0]), 6 * x.shape[0]))
 
     x, y, z = np.where(~mask[:,:-1,:] & mask[:,1:,:])
     vertices = [
@@ -173,6 +187,7 @@ def create_vertices(voxels_array):
         x, y + 1, z + 1
     ]
     arrays.append(np.array(vertices).transpose().flatten())
+    normals.append(np.tile(np.array([0, -1, 0]), 6 * x.shape[0]))
 
     # Z
     x, y, z = np.where(mask[:,:,:-1] & ~mask[:,:,1:])
@@ -186,6 +201,7 @@ def create_vertices(voxels_array):
         x, y + 1, z + 1
     ]
     arrays.append(np.array(vertices).transpose().flatten())
+    normals.append(np.tile(np.array([0, 0, 1]), 6 * x.shape[0]))
 
     x, y, z = np.where(~mask[:,:,:-1] & mask[:,:,1:])
     vertices = [
@@ -198,9 +214,9 @@ def create_vertices(voxels_array):
         x, y + 1, z + 1
     ]
     arrays.append(np.array(vertices).transpose().flatten())
+    normals.append(np.tile(np.array([0, 0, -1]), 6 * x.shape[0]))
 
-    result = np.concatenate(arrays).astype(np.float32)
-    return result
+    return np.concatenate(arrays).astype(np.float32), np.concatenate(normals).astype(np.float32)
 
 FILENAME = '/home/marian/shapenet/ShapeNetCore.v2/02747177/2f00a785c9ac57c07f48cf22f91f1702/models/model_normalized.solid.binvox'
 
@@ -213,7 +229,6 @@ voxels = voxels.data[::2, ::2, ::2].astype(np.float32) # 128^3 to 64^3, bool to 
 voxel_shape = [voxels.shape[0] + 2, voxels.shape[2] + 2, voxels.shape[2] + 2]
 voxel_size = max(voxel_shape)
 
-vertices = create_vertices(voxels)
-vertices = vertices
+vertices, normals = create_vertices(voxels)
 
 main()
