d09606bb419307f5c5030342cdd481be1957daee
Marian <mail@marian42.de>
2019-06-01 00:36:37 +0200

    Use new dataset and inception score in the Wasserstein GAN

--- wgan.py
+++ wgan.py
@@ -12,9 +12,7 @@ from model import Generator, Discriminator, Autoencoder
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-dataset = torch.load("data/chairs-32.to").to(device)
-dataset = dataset
-dataset_size = dataset.shape[0]
+from dataset import dataset as dataset
 
 show_viewer = "nogui" not in sys.argv
 
@@ -24,18 +22,20 @@ if show_viewer:
 
 generator = Generator()
 generator.filename = "wgan-generator.to"
-generator.load()
 
 critic = Discriminator()
 critic.filename = "wgan-critic.to"
 critic.use_sigmoid = False
-critic.load()
 
-def load_from_autoencoder():
+if "continue" in sys.argv:
+    generator.load()
+    critic.load()
+
+if "copy_autoencoder_weights" in sys.argv:
     autoencoder = Autoencoder()
     autoencoder.load()
     generator.copy_autoencoder_weights(autoencoder)
-#load_from_autoencoder()
+
 
 LEARN_RATE = 0.00005
 BATCH_SIZE = 64
@@ -45,6 +45,7 @@ CRITIC_WEIGHT_LIMIT = 0.01
 generator_optimizer = optim.RMSprop(generator.parameters(), lr=LEARN_RATE)
 critic_optimizer = optim.RMSprop(critic.parameters(), lr=LEARN_RATE)
 
+print('Inception score: {:.4f}'.format(generator.get_inception_score(device)))
 
 def create_batches(sample_count, batch_size):
     batch_count = int(sample_count / batch_size)
@@ -61,7 +62,7 @@ def train():
     for epoch in count():
         batch_index = 0
         epoch_start_time = time.time()
-        for batch in create_batches(dataset_size, BATCH_SIZE):
+        for batch in create_batches(dataset.size, BATCH_SIZE):
             try:
                 # train critic
                 indices = torch.tensor(batch, device = device)
@@ -70,8 +71,8 @@ def train():
                 generator.zero_grad()
                 critic.zero_grad()
 
-                valid_sample = dataset[indices, :, :, :]                
-                fake_sample = generator.generate(device, batch_size = current_batch_size).detach()
+                valid_sample = dataset.voxels[indices, :, :, :]                
+                fake_sample = generator.generate(device, sample_size = current_batch_size).detach()
                 fake_critic_output = critic.forward(fake_sample)
                 valid_critic_output = critic.forward(valid_sample)
                 critic_loss = -(torch.mean(valid_critic_output) - torch.mean(fake_critic_output))
@@ -84,7 +85,7 @@ def train():
                     generator.zero_grad()
                     critic.zero_grad()
                        
-                    fake_sample = generator.generate(device, batch_size = BATCH_SIZE)
+                    fake_sample = generator.generate(device, sample_size = BATCH_SIZE)
                     if show_viewer:
                         viewer.set_voxels(fake_sample[0, :, :, :].squeeze().detach().cpu().numpy())
                     fake_critic_output = critic.forward(fake_sample)
@@ -95,9 +96,10 @@ def train():
                 
                     fake_sample_prediction = torch.mean(fake_critic_output).item()
                     valid_sample_prediction = torch.mean(valid_critic_output).item()
-                    print("epoch " + str(epoch) + ", batch " + str(batch_index) \
-                        + ": fake value: " + '{0:.1f}'.format(fake_sample_prediction) \
-                        + ", valid value: " + '{0:.1f}'.format(valid_sample_prediction))
+                    if "verbose" in sys.argv:
+                        print("epoch " + str(epoch) + ", batch " + str(batch_index) \
+                            + ": fake value: " + '{0:.1f}'.format(fake_sample_prediction) \
+                            + ", valid value: " + '{0:.1f}'.format(valid_sample_prediction))
                 batch_index += 1                
             except KeyboardInterrupt:
                 if show_viewer:
@@ -106,7 +108,12 @@ def train():
         
         generator.save()
         critic.save()
-        print("Model parameters saved. Epoch took " + '{0:.1f}'.format(time.time() - epoch_start_time) + "s.")
+        print('Epoch {:d} ({:.1f}s), inception score: {:.4f}, critic values: {:.2f}, {:.2f}'.format(
+            epoch,
+            time.time() - epoch_start_time,
+            generator.get_inception_score(device, sample_size=800),
+            fake_sample_prediction,
+            valid_sample_prediction))
 
 
 train()                
