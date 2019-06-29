892eb7363fd21ffe5586684c944e2086d85e1ecc
Marian <mail@marian42.de>
2019-06-29 17:22:35 +0200

    First working version of pointcloud reconstrruction from a render

--- sdf/mesh_to_sdf.py
+++ sdf/mesh_to_sdf.py
@@ -22,15 +22,13 @@ class CustomShaderCache():
             self.program = pyrender.shader_program.ShaderProgram("sdf/shaders/mesh.vert", "sdf/shaders/mesh.frag", defines=defines)
         return self.program
 
-def get_rotation_matrix(angle):
-    rotation = Rotation.from_euler('y', angle, degrees=True)
+def get_rotation_matrix(angle, axis='y'):
+    rotation = Rotation.from_euler(axis, angle, degrees=True)
     matrix = np.identity(4)
     matrix[:3, :3] = rotation.as_dcm()
     return matrix
 
 mesh = trimesh.load(PATH)
-
-test = 0.5
 
 def get_points(angle):
     camera_pose = np.identity(4)
@@ -39,44 +37,38 @@ def get_points(angle):
 
     scene = pyrender.Scene(bg_color=(1.0, 1.0, 1.0, 1.0), ambient_light=np.ones(4) * 0.1)
     scene.add(pyrender.Mesh.from_trimesh(mesh, smooth = False))
-    camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0, aspectRatio=1.0, znear=0.001, zfar = 2)
+    camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0, aspectRatio=1.0, znear=0.001, zfar = 200)
     scene.add(camera, pose=camera_pose)
 
     renderer = pyrender.OffscreenRenderer(VIEWPORT_SIZE, VIEWPORT_SIZE)
     renderer._renderer._program_cache = CustomShaderCache()
 
-    camera_direction = np.matmul(camera_pose, np.array([0, 0, 1, 0]))[:3]
-
     color, depth = renderer.render(scene)
-    indices = np.argwhere(depth != 0)
+    indices = np.argwhere(depth != 1)
 
     normals = color[indices[:, 0], indices[:, 1]] / 255 * 2 - 1
+    camera_direction = np.matmul(camera_pose, np.array([0, 0, 1, 0]))[:3]
     normal_orientation = np.dot(normals, camera_direction)
     normals[normal_orientation < 0] *= -1
 
     points = np.ones((indices.shape[0], 4))
-    points[:, :2] = indices.astype(float) / VIEWPORT_SIZE * 2 - 1
-    points[:, 2] = depth[indices[:, 0], indices[:, 1]] * test
+    points[:, [1, 0]] = indices.astype(float) / VIEWPORT_SIZE * 2 - 1
+    points[:, 1] *= -1
+    points[:, 2] = depth[indices[:, 0], indices[:, 1]]
+    
+    scale = np.identity(4) * 0.5
+    scale[3, 3] = 1
+    matrix = np.matmul(np.matmul(camera_pose, scale), np.linalg.inv(camera.get_projection_matrix()))
 
-    world_to_clipping = np.matmul(camera.get_projection_matrix(), np.linalg.inv(camera_pose))
-    inverted = np.linalg.inv(world_to_clipping)
-
-    points = np.matmul(inverted, points.transpose()).transpose()
+    points = np.matmul(points, matrix.transpose())
     points /= points[:, 3][:, np.newaxis]
+
     return points[:, :3]
 
 
-points = np.concatenate((get_points(135), get_points(145)))
+points = np.concatenate((get_points(0), get_points(90)))
 pyrender_mesh = pyrender.Mesh.from_points(points)
 scene = pyrender.Scene()
-node = scene.add(pyrender_mesh)
+#scene.add(pyrender.Mesh.from_trimesh(mesh, smooth = False))
+scene.add(pyrender_mesh)
 viewer = pyrender.Viewer(scene, use_raymond_lighting=True)
-
-
-
-if False:
-    img = Image.fromarray(depth / np.max(depth) * 255, 'F')
-    img.show()
-
-    img = Image.fromarray(color, 'RGB')
-    img.show()
