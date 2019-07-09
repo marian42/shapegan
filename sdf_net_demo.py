9096e0daf5d23f02ba75b9d0bba90598d1020b2e
Marian <mail@marian42.de>
2019-07-09 21:29:23 +0200

    Fix some bugs related to image sequence generation

--- sdf_net_demo.py
+++ sdf_net_demo.py
@@ -4,6 +4,8 @@ import time
 import numpy as np
 import sys
 import random
+from tqdm import tqdm
+import cv2
 
 from voxel.viewer import VoxelViewer
 from model import SDFNet, LATENT_CODE_SIZE, LATENT_CODES_FILENAME
@@ -18,7 +20,7 @@ latent_codes = torch.load(LATENT_CODES_FILENAME).to(device)
 MODEL_COUNT = latent_codes.shape[0]
 TRANSITION_FRAMES = 40
 WAIT_FRAMES = 10
-MODEL_COUNT = 30
+SAMPLE_COUNT = 4
 
 WAIT_TIME = 0.8
 TRANSITION_TIME = 0.4
@@ -26,28 +28,26 @@ TRANSITION_TIME = 0.4
 def get_random_latent_code():
     return latent_codes[random.randrange(MODEL_COUNT), :]
 
-frame_index = 0
-
-previous_model = None
 start_model = get_random_latent_code()
-
 
 def save_image(image, frame_index):
     cv2.imwrite("images/frame-{:05d}.png".format(frame_index), image)
 
 def create_image_sequence():
+    previous_model = None
+    frame_index = 0
     viewer = VoxelViewer(start_thread=False)
-    progress_bar = tqdm(total=MODEL_COUNT * (TRANSITION_FRAMES + WAIT_FRAMES))
+    progress_bar = tqdm(total=SAMPLE_COUNT * (TRANSITION_FRAMES + 1))
     next_model = start_model
 
-    for model_index in range(MODEL_COUNT):
+    for sample_index in range(SAMPLE_COUNT):
         previous_model = next_model
-        if model_index == MODEL_COUNT - 1:
+        if sample_index == SAMPLE_COUNT - 1:
             next_model = start_model
         else:
             next_model = get_random_latent_code()
 
-        for step in tqdm(list(range(TRANSITION_FRAMES))):
+        for step in range(TRANSITION_FRAMES):
             progress = step / TRANSITION_FRAMES
             latent_code = previous_model * (1 - progress) + next_model * progress
             
@@ -60,13 +60,16 @@ def create_image_sequence():
             frame_index += 1
             progress_bar.update()
         
+        image = viewer.get_image(crop = False, output_size = 800)
         for _ in range(WAIT_FRAMES):
-            image = viewer.get_image(crop = False, output_size = 800)
             save_image(image, frame_index)
             frame_index += 1
-            progress_bar.update()
+
+    print("\n\nUse this command to create a video:\n")
+    print('ffmpeg -framerate 24 -i images/frame-%05d.png -c:v libx264 -profile:v high -crf 19 -pix_fmt yuv420p video.mp4')
 
 def show_models():
+    previous_model = None
     viewer = VoxelViewer()
     next_model = start_model
 
