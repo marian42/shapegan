b58003ffd6bcca2b07262b6fcdb5f3071f7b4f5d
Marian <mail@marian42.de>
2019-07-19 03:40:55 +0200

    Improve SDF GAN debug output

--- sdf_gan.py
+++ sdf_gan.py
@@ -28,8 +28,8 @@ generator.to(device)
 discriminator = SDFDiscriminator()
 discriminator.to(device)
 
-discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=1e-6)
 generator_optimizer = torch.optim.Adam(generator.parameters(), lr=1e-4)
+discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=1e-6)
 
 discriminator_criterion = torch.nn.functional.mse_loss
 
@@ -46,8 +46,11 @@ def get_points_in_unit_sphere(n = POINT_SAMPLE_COUNT):
     x = x[mask, :]
     return x
 
+debug_points = get_points_in_unit_sphere(n = 10)
+debug_latent_codes = create_latent_code(repeat=debug_points.shape[0])
 
-history = deque(maxlen=50)
+history_fake = deque(maxlen=50)
+history_real = deque(maxlen=50)
 viewer = VoxelViewer()
 
 for step in count():
@@ -59,7 +62,6 @@ for step in count():
     test_points = get_points_in_unit_sphere()
     fake_sdf = generator.forward(test_points, code)
     fake_discriminator_assessment = discriminator.forward(test_points, fake_sdf)
-    history.append(fake_discriminator_assessment.item())
     loss = -torch.log(fake_discriminator_assessment)
     loss.backward()
     generator_optimizer.step()
@@ -72,6 +74,7 @@ for step in count():
     indices += POINTCLOUD_SIZE * random.randint(0, MODEL_COUNT - 1)
     
     output = discriminator.forward(points[indices, :], sdf[indices])
+    history_real.append(output.item())    
     loss = discriminator_criterion(output, torch.tensor(1, device=device, dtype=torch.float32))
     loss.backward()
     discriminator_optimizer.step()
@@ -85,15 +88,17 @@ for step in count():
     with torch.no_grad():
         fake_sdf = generator.forward(test_points, code)
     output = discriminator.forward(test_points, fake_sdf)
+    history_fake.append(output.item())
 
     loss = discriminator_criterion(output, torch.tensor(0, device=device, dtype=torch.float32))
     loss.backward()
     discriminator_optimizer.step()
 
 
-    if step % 50 == 0:
-        print(np.mean(history))
+    if step % 50 == 0 and step != 0:
+        print("Prediction on fake samples: {:.3f}, on real samples: {:.3f}".format(np.mean(history_fake), np.mean(history_real)))
         try:
             viewer.set_mesh(generator.get_mesh(create_latent_code(repeat=1), device))
         except ValueError:
             print("(Voxel volume contains no sign changes)")
+        print(generator.forward(debug_points, debug_latent_codes).detach())
