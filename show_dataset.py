b88455414439d454d52a51f9b491d662376a0b8c
Marian <mail@marian42.de>
2019-07-23 13:41:33 +0200

    Add an option to show meshes in the dataset viewer

--- show_dataset.py
+++ show_dataset.py
@@ -1,16 +1,37 @@
 import random
 import torch
 import time
+import sys
+from tqdm import tqdm
 
 from voxel.viewer import VoxelViewer
-from dataset import dataset as dataset
+
+PATH = '/home/marian/shapenet/ShapeNetCore.v2/03001627/'
 
 viewer = VoxelViewer()
 
-while True:
-    try:
-        viewer.set_voxels(dataset.voxels[random.randint(0, dataset.size - 1), :, :, :].squeeze().detach().cpu().numpy())
-        time.sleep(1)
-    except KeyboardInterrupt:
-        viewer.stop()
-        break
+if "mesh" in sys.argv:
+    import trimesh
+    import os
+
+    model_filenames = []
+    for directory, _, files in os.walk(PATH):
+        model_filename = os.path.join(directory, 'model_normalized.obj')
+        if os.path.isfile(model_filename):
+            model_filenames.append(model_filename)
+
+    while True:
+        filename = random.choice(model_filenames)
+        mesh = trimesh.load(filename)
+        viewer.set_mesh(mesh, center_and_scale=True)
+        time.sleep(2)
+else:
+    from dataset import dataset as dataset
+
+    for i in tqdm(list(range(dataset.size))):
+        try:
+            viewer.set_voxels(dataset.voxels[i, :, :, :].squeeze().detach().cpu().numpy(), use_marching_cubes=False)
+            time.sleep(2)
+        except KeyboardInterrupt:
+            viewer.stop()
+            break
