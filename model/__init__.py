53da9ac59aa4ed07342285da857ee51f868983f2
Marian <mail@marian42.de>
2019-09-17 12:42:07 +0200

    Create checkpoints directory if it doesn't exist

--- model/__init__.py
+++ model/__init__.py
@@ -5,6 +5,7 @@ from torch.nn import Sequential, Linear, ReLU, BatchNorm1d
 import os
 
 MODEL_PATH = "models"
+CHECKPOINT_PATH = os.path.join(MODEL_PATH, 'checkpoints')
 LATENT_CODES_FILENAME = os.path.join(MODEL_PATH, "sdf_net_latent_codes.to")
 LATENT_CODE_SIZE = 64
 
@@ -30,13 +31,15 @@ class SavableModule(nn.Module):
             filename = filename.split('.')
             filename[-2] += '-epoch-{:05d}'.format(epoch)
             filename = '.'.join(filename)
-            return os.path.join(MODEL_PATH, 'checkpoints', filename)
+            return os.path.join(CHECKPOINT_PATH, filename)
 
 
     def load(self, epoch=None):
         self.load_state_dict(torch.load(self.get_filename(epoch=epoch)), strict=False)
     
     def save(self, epoch=None):
+        if epoch is not None and not os.path.exists(CHECKPOINT_PATH):
+            os.mkdir(CHECKPOINT_PATH)
         torch.save(self.state_dict(), self.get_filename(epoch=epoch))
 
     @property
