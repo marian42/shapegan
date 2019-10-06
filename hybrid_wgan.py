51b9c895cb4696d44bd65050ed3d18be1bd4d1cf
Marian <mail@marian42.de>
2019-10-06 23:56:46 +0200

    Fix epoch counting when contnuing hybrid WGAN training

--- hybrid_wgan.py
+++ hybrid_wgan.py
@@ -39,7 +39,13 @@ if "continue" in sys.argv:
     generator.load()
     critic.load()
 
-log_file = open("plots/hybrid_wgan_training.csv", "a" if "continue" in sys.argv else "w")
+LOG_FILE_NAME = "plots/hybrid_wgan_training.csv"
+first_epoch = 0
+if 'continue' in sys.argv:
+    log_file_contents = open(LOG_FILE_NAME, 'r').readlines()
+    first_epoch = len(log_file_contents)
+
+log_file = open(LOG_FILE_NAME, "a" if "continue" in sys.argv else "w")
 
 generator_optimizer = optim.Adam(generator.parameters(), lr=LEARN_RATE)
 
@@ -88,7 +94,7 @@ history_fake = deque(maxlen=50)
 history_real = deque(maxlen=50)
 
 def train():
-    for epoch in count():
+    for epoch in count(start=first_epoch):
         batch_index = 0
         epoch_start_time = time.time()
         for batch in tqdm(list(create_batches(dataset.size, BATCH_SIZE))):
