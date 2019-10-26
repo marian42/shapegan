e92a6c78d49c23954c36f51ba9b9ced6fd75bfcd
Marian <mail@marian42.de>
2019-10-27 00:23:18 +0200

    Add an option to save an image sequence in the training demo

--- demo_training.py
+++ demo_training.py
@@ -6,6 +6,8 @@ from util import device
 
 from model.sdf_net import SDFNet
 from rendering import MeshRenderer
+import sys
+import cv2
 latent_code_size = 0
 
 MODEL_PATH = 'examples/chair.obj'
@@ -15,14 +17,19 @@ mesh = scale_to_unit_sphere(mesh)
 mesh_sdf = MeshSDF(mesh)
 points, sdf = mesh_sdf.get_sample_points()
 
+save_images = 'save' in sys.argv
+
+if save_images:
+    viewer = MeshRenderer(start_thread=False, size=1080)
+else:
+    viewer = MeshRenderer()
+
 points = torch.tensor(points, dtype=torch.float32, device=device)
 sdf = torch.tensor(sdf, dtype=torch.float32, device=device)
 sdf.clamp_(-0.1, 0.1)
 
-viewer = MeshRenderer()
-
 sdf_net = SDFNet(latent_code_size=latent_code_size).to(device)
-optimizer = torch.optim.Adam(sdf_net.parameters(), lr=1e-4)
+optimizer = torch.optim.Adam(sdf_net.parameters(), lr=1e-5)
 
 BATCH_SIZE = 20000
 latent_code = torch.zeros((BATCH_SIZE, latent_code_size), device=device)
@@ -32,6 +39,9 @@ positive_indices = (sdf > 0).nonzero().squeeze().cpu().numpy()
 negative_indices = (sdf < 0).nonzero().squeeze().cpu().numpy()
 
 step = 0
+error_targets = np.logspace(np.log10(0.02), np.log10(0.0005), num=500)
+image_index = 0
+
 while True:
     try:
         #indices.random_(points.shape[0] - 1)
@@ -42,13 +52,20 @@ while True:
         predicted_sdf = sdf_net.forward(points[indices, :], latent_code)
         batch_sdf = sdf[indices]
         loss = torch.mean(torch.abs(predicted_sdf - batch_sdf))
-        print(step, loss.item())
         loss.backward()
         optimizer.step()
 
-        if step % 10 == 0:
-            viewer.set_mesh(sdf_net.get_mesh(latent_code[0, :], voxel_resolution=192))
+        if loss.item() < error_targets[image_index]:
+            try:
+                viewer.set_mesh(sdf_net.get_mesh(latent_code[0, :], voxel_resolution=64, raise_on_empty=True))
+                if save_images:
+                    image = viewer.get_image(flip_red_blue=True)
+                    cv2.imwrite("images/frame-{:05d}.png".format(image_index), image)
+                image_index += 1
+            except ValueError:
+                pass
         step += 1
+        print('Step {:04d}, Image {:04d}, loss: {:.6f}, target: {:.6f}'.format(step, image_index, loss.item(), error_targets[image_index]))
     except KeyboardInterrupt:
         viewer.stop()
         break
