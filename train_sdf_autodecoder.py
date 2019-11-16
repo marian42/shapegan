2619b402233d7533fe86bf34f0ba768de204e0ad
Marian <mail@marian42.de>
2019-11-16 19:30:59 +0100

    Remove part handling from autodecoder training

--- train_sdf_autodecoder.py
+++ train_sdf_autodecoder.py
@@ -13,15 +13,16 @@ import sys
 from model.sdf_net import SDFNet, LATENT_CODE_SIZE, LATENT_CODES_FILENAME
 from util import device
 
-from dataset import dataset
-dataset.load_labels(device=device)
-dataset.rescale_sdf = False
-
 if "nogui" not in sys.argv:
     from rendering import MeshRenderer
     viewer = MeshRenderer()
 
-POINTCLOUD_PART_SIZE = 200000 // dataset.sdf_part_count
+POINTCLOUD_SIZE = 200000
+
+points = torch.load('data/sdf_points.to').to(device)
+sdf = torch.load('data/sdf_values.to').to(device)
+
+MODEL_COUNT = points.shape[0] // POINTCLOUD_SIZE
 BATCH_SIZE = 20000
 SDF_CUTOFF = 0.1
 
@@ -35,7 +36,7 @@ if "continue" in sys.argv:
     latent_codes = torch.load(LATENT_CODES_FILENAME).to(device)
 else:    
     normal_distribution = torch.distributions.normal.Normal(0, 0.0001)
-    latent_codes = normal_distribution.sample((dataset.size, LATENT_CODE_SIZE)).to(device)
+    latent_codes = normal_distribution.sample((MODEL_COUNT, LATENT_CODE_SIZE)).to(device)
 latent_codes.requires_grad = True
 
 network_optimizer = optim.Adam(sdf_net.parameters(), lr=1e-5)
@@ -50,7 +51,7 @@ if 'continue' in sys.argv:
 log_file = open(LOG_FILE_NAME, "a" if "continue" in sys.argv else "w")
 
 def create_batches():
-    size = POINTCLOUD_PART_SIZE * dataset.size
+    size = MODEL_COUNT * POINTCLOUD_SIZE
     batch_count = int(size / BATCH_SIZE)
     indices = np.arange(size)
     np.random.shuffle(indices)
@@ -61,45 +62,39 @@ def create_batches():
 def train():
     for epoch in count(start=first_epoch):
         epoch_start_time = time.time()
-        progress = tqdm(total=dataset.sdf_part_count * POINTCLOUD_PART_SIZE * dataset.size // BATCH_SIZE)
         loss_values = []
         batch_index = 0
-        for part_index in range(dataset.sdf_part_count):
-            dataset_part = dataset.load_sdf_part(part_index)
-            for batch in list(create_batches()):
-                indices = torch.tensor(batch, device = device)
-                model_indices = indices / POINTCLOUD_PART_SIZE
+        for batch in tqdm(list(create_batches())):
+            indices = torch.tensor(batch, device = device)
+            model_indices = indices / POINTCLOUD_SIZE
 
-                batch_latent_codes = latent_codes[model_indices, :]
-                batch_points = dataset_part.points[indices, :]
-                batch_sdf = dataset_part.values[indices]
+            batch_latent_codes = latent_codes[model_indices, :]
+            batch_points = points[indices, :]
+            batch_sdf = sdf[indices]
 
-                sdf_net.zero_grad()
-                if latent_codes.grad is not None:
-                    latent_codes.grad.data.zero_()
-                output = sdf_net.forward(batch_points, batch_latent_codes)
-                output = output.clamp(-SDF_CUTOFF, SDF_CUTOFF)
-                loss = torch.mean(torch.abs(output - batch_sdf)) + SIGMA * torch.mean(torch.pow(batch_latent_codes, 2))
-                loss.backward()
-                network_optimizer.step()
-                latent_code_optimizer.step()
-                loss_values.append(loss.item())
+            sdf_net.zero_grad()
+            if latent_codes.grad is not None:
+                latent_codes.grad.data.zero_()
+            output = sdf_net.forward(batch_points, batch_latent_codes)
+            output = output.clamp(-SDF_CUTOFF, SDF_CUTOFF)
+            loss = torch.mean(torch.abs(output - batch_sdf)) + SIGMA * torch.mean(torch.pow(batch_latent_codes, 2))
+            loss.backward()
+            network_optimizer.step()
+            latent_code_optimizer.step()
+            loss_values.append(loss.item())
 
-                if batch_index % 1000 == 0 and "nogui" not in sys.argv:
-                    try:
-                        viewer.set_mesh(sdf_net.get_mesh(latent_codes[random.randrange(dataset.size), :]))
-                    except ValueError:
-                        pass
+            if batch_index % 400 == 0 and "nogui" not in sys.argv:
+                try:
+                    viewer.set_mesh(sdf_net.get_mesh(latent_codes[random.randrange(MODEL_COUNT), :]))
+                except ValueError:
+                    pass
 
-                batch_index += 1
-                progress.update()
-        progress.close()
+            batch_index += 1
 
         variance = np.var(latent_codes.detach().reshape(-1).cpu().numpy()) ** 0.5
-        score = sdf_net.get_inception_score(latent_variance = variance)
         epoch_duration = time.time() - epoch_start_time
         
-        print("Epoch {:d}, {:.1f}s. Loss: {:.8f}, Inception score: {:.4}".format(epoch, epoch_duration, np.mean(loss_values), score))
+        print("Epoch {:d}, {:.1f}s. Loss: {:.8f}".format(epoch, epoch_duration, np.mean(loss_values)))
 
         sdf_net.save()
         torch.save(latent_codes, LATENT_CODES_FILENAME)
@@ -107,7 +102,7 @@ def train():
         sdf_net.save(epoch=epoch)
         torch.save(latent_codes, sdf_net.get_filename(epoch=epoch, filename='sdf_net_latent_codes.to'))
 
-        log_file.write('{:d} {:.1f} {:.6f} {:.6f} {:.6f}\n'.format(epoch, epoch_duration, np.mean(loss_values), score, variance))
+        log_file.write('{:d} {:.1f} {:.6f} {:.6f}\n'.format(epoch, epoch_duration, np.mean(loss_values), variance))
         log_file.flush()
 
 train()
