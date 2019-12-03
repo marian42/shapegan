58f70bb7d3eb85efab68d570b50d859efe7a2c4b
rusty1s <matthias.fey@tu-dortmund.de>
2019-12-03 09:58:32 +0100

    refinement generator

--- point_sdf/shape_gan_ref.py
+++ point_sdf/shape_gan_ref.py
@@ -1,33 +1,71 @@
+import os.path as osp
+import sys
+
+import argparse
 import torch
 from torch.utils.data import DataLoader
 from torch.optim import RMSprop
 
-from shapenet import ShapeNetSDF
+from shapenet import ShapeNetPointSDF, visualize
 from generator import SDFGenerator
-from discriminator import Encoder
+from pointnet import PointNet
 
-LATENT_DIM = 128
+parser = argparse.ArgumentParser()
+parser.add_argument('--category', type=str, required=True)
+parser.add_argument('--G_init', type=str, required=True)
+parser.add_argument('--D_init', type=str, required=True)
+parser.add_argument('--level', type=float, default=0.04)
+parser.add_argument('--eval', action='store_true')
+args = parser.parse_args()
+
+LATENT_SIZE = 128
 GRADIENT_PENALITY = 10
+HIDDEN_SIZE = 256
+NUM_LAYERS = 8
+NORM = True
 THRESHOLD = 0.1
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+G = SDFGenerator(LATENT_SIZE, HIDDEN_SIZE, NUM_LAYERS, NORM, dropout=0.0)
+D = PointNet(out_channels=1)
+G, D = G.to(device), D.to(device)
+G.load_state_dict(torch.load(args.G_init, map_location=device))
+D.load_state_dict(torch.load(args.D_init, map_location=device))
+G_optimizer = RMSprop(G.parameters(), lr=0.0001)
+D_optimizer = RMSprop(D.parameters(), lr=0.0001)
+
+if args.eval:
+    path = osp.join(args.category, 'Gref.pt')
+    G.load_state_dict(torch.load(path, map_location=device))
+    G.eval()
+    torch.manual_seed(12345)
+    for _ in range(5):
+        pos = 2 * torch.rand((64 * 1024, 3), device=device) - 1
+        pos.requires_grad_(True)
+        z = torch.randn((LATENT_SIZE, ), device=device)
+        dist = G(pos, z).squeeze()
+        visualize(pos.detach(), dist.detach(), dist.detach().abs() < 0.05)
+
+        mesh = G.get_mesh(z, 64, level=args.level)
+        visualize(mesh.sample(2048))
+
+root = '/data/SDF_GAN'
+dataset = ShapeNetPointSDF(root, category=args.category, split='train')
 
 
 def generate_batch(u_pos, u_dist, s_pos, s_dist):
-    u_batch = torch.stack([
-        torch.full((u_pos.size(1), ), i, device=device, dtype=torch.long)
-        for i in range(u_pos.size(0))
-    ], dim=0)
+    u_batch = torch.arange(u_pos.size(0), device=u_pos.device)
+    u_batch = u_batch.view(-1, 1).repeat(1, u_pos.size(1))
 
     mask = u_dist.abs().squeeze(-1) < THRESHOLD
 
-    s_pos = s_pos[mask]
-    s_dist = s_dist[mask]
-    s_batch = u_batch[mask]
+    s_pos = s_pos[mask].view(-1, 3)
+    s_dist = s_dist[mask].view(-1, 1)
+    s_batch = u_batch[mask].view(-1)
 
-    u_pos, u_dist = u_pos.view(-1, 3), u_dist.view(-1, 1)
+    u_pos = u_pos.view(-1, 3)
+    u_dist = u_dist.view(-1, 1)
     u_batch = u_batch.view(-1)
-
-    s_pos, s_dist = s_pos.view(-1, 3), s_dist.view(-1, 1)
-    s_batch = s_batch.view(-1)
 
     return (
         torch.cat([u_pos, s_pos], dim=0),
@@ -41,96 +79,94 @@ class RefinementGenerator(torch.nn.Module):
         super(RefinementGenerator, self).__init__()
         self.generator = generator
 
-    def forward(self, pos, z):
-        pos.requires_grad_(True)
-        dist = self.generator(pos, z)
+    def forward(self, u_pos, z):
+        u_pos.requires_grad_(True)
+        u_dist = self.generator(u_pos, z)
 
-        grad = torch.autograd.grad(dist, pos,
-                                   grad_outputs=torch.ones_like(dist),
+        grad = torch.autograd.grad(u_dist, u_pos,
+                                   grad_outputs=torch.ones_like(u_dist),
                                    retain_graph=True, only_inputs=True)[0]
-        ref_pos = pos - dist * grad
-        ref_dist = self.generator(pos, z)
+        s_pos = u_pos - u_dist * grad
+        s_pos = s_pos + 0.0025 * torch.randn_like(s_pos)
+        s_dist = self.generator(s_pos, z)
 
-        return pos, dist, ref_pos, ref_dist
+        return u_pos, u_dist, s_pos, s_dist
 
 
-root = '/data/sdf_chairs/chairs'
-dataset = ShapeNetSDF(root, num_points=1024 * 8)
-loader = DataLoader(dataset, batch_size=8, shuffle=True, num_workers=6)
+ref_G = RefinementGenerator(G).to(device)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-G = SDFGenerator(LATENT_DIM, 256, num_layers=8, dropout=0.0)
-G = RefinementGenerator(G)
-D = Encoder(1)
-G, D = G.to(device), D.to(device)
-
-G_optimizer = RMSprop(G.parameters(), lr=0.0001)
-D_optimizer = RMSprop(D.parameters(), lr=0.0001)
+configuration = [
+    (4096, 24, 300),
+    (8192, 12, 300),
+    (16384, 6, 900),
+]
 
 num_steps = 0
-for epoch in range(1, 1001):
-    total_loss = 0
-    for uniform, surface in loader:
-        num_steps += 1
+for num_points, batch_size, epochs in configuration:
+    dataset.num_points = num_points
+    loader = DataLoader(dataset, batch_size, shuffle=True, num_workers=6)
 
-        uniform, surface = uniform.to(device), surface.to(device)
-        u_pos, u_dist = uniform[..., :3], uniform[..., 3:]
-        s_pos, s_dist = surface[..., :3], surface[..., 3:]
+    for epoch in range(1, epochs + 1):
+        total_loss = 0
+        for uniform, surface in loader:
+            num_steps += 1
 
-        real_pos, real_dist, real_batch = generate_batch(
-            u_pos, u_dist, s_pos, s_dist)
+            uniform, surface = uniform.to(device), surface.to(device)
+            u_pos, u_dist = uniform[..., :3], uniform[..., 3:]
+            s_pos, s_dist = surface[..., :3], surface[..., 3:]
 
-        D_optimizer.zero_grad()
-
-        z = torch.randn(uniform.size(0), LATENT_DIM, device=device)
-        u_pos2, u_dist2, s_pos2, s_dist2 = G(u_pos, z)
-        fake_pos, fake_dist, fake_batch = generate_batch(
-            u_pos2, u_dist2, s_pos2, s_dist2)
-
-        out_real = D(real_pos, real_dist, real_batch)
-        out_fake = D(fake_pos, fake_dist, fake_batch)
-        D_loss = out_fake.mean() - out_real.mean()
-
-        alpha = torch.rand((uniform.size(0), 1, 1), device=device)
-        interpolated = alpha * u_dist + (1 - alpha) * u_dist2
-        interpolated.requires_grad_(True)
-        batch = torch.cat([
-            torch.full((u_pos.size(1), ), i, device=device, dtype=torch.long)
-            for i in range(u_pos.size(0))
-        ], dim=0)
-        out = D(u_pos.view(-1, 3), interpolated.view(-1, 1), batch)
-
-        grad = torch.autograd.grad(out, interpolated,
-                                   grad_outputs=torch.ones_like(out),
-                                   create_graph=True, retain_graph=True,
-                                   only_inputs=True)[0]
-        grad_norm = grad.view(grad.size(0), -1).norm(dim=-1, p=2)
-        gp = GRADIENT_PENALITY * ((grad_norm - 1).pow(2).mean())
-
-        loss = D_loss + gp
-        loss.backward()
-        D_optimizer.step()
-
-        if num_steps % 5 == 0:
-            G_optimizer.zero_grad()
-            z = torch.randn(uniform.size(0), LATENT_DIM, device=device)
-            pos = 2 * torch.rand_like(u_pos) - 1
-            z = torch.randn(uniform.size(0), LATENT_DIM, device=device)
-            u_pos, u_dist, s_pos, s_dist = G(pos, z)
-            fake_pos, fake_dist, fake_batch = generate_batch(
+            real_pos, real_dist, real_batch = generate_batch(
                 u_pos, u_dist, s_pos, s_dist)
+
+            D_optimizer.zero_grad()
+
+            z = torch.randn(uniform.size(0), LATENT_SIZE, device=device)
+            fake_u_pos, fake_u_dist, fake_s_pos, fake_s_dist = ref_G(u_pos, z)
+            fake_pos, fake_dist, fake_batch = generate_batch(
+                fake_u_pos, fake_u_dist, fake_s_pos, fake_s_dist)
+
+            out_real = D(real_pos, real_dist, real_batch)
             out_fake = D(fake_pos, fake_dist, fake_batch)
-            loss = -out_fake.mean()
+            D_loss = out_fake.mean() - out_real.mean()
+
+            alpha = torch.rand((uniform.size(0), 1, 1), device=device)
+            interpolated = alpha * u_dist + (1 - alpha) * fake_u_dist
+            interpolated.requires_grad_(True)
+            out = D(u_pos, interpolated)
+
+            grad = torch.autograd.grad(out, interpolated,
+                                       grad_outputs=torch.ones_like(out),
+                                       create_graph=True, retain_graph=True,
+                                       only_inputs=True)[0]
+            grad_norm = grad.view(grad.size(0), -1).norm(dim=-1, p=2)
+            gp = GRADIENT_PENALITY * ((grad_norm - 1).pow(2).mean())
+
+            loss = D_loss + gp
             loss.backward()
-            G_optimizer.step()
+            D_optimizer.step()
 
-            print('D: {:.4f}, GP: {:.4f}, F: {:.4f} - {:.4f}'.format(
-                -D_loss.item(), gp.item(),
-                fake_dist.min().item(),
-                fake_dist.max().item()))
-        total_loss += D_loss.abs().item()
+            if num_steps % 5 == 0:
+                G_optimizer.zero_grad()
+                z = torch.randn(uniform.size(0), LATENT_SIZE, device=device)
+                fake = ref_G(u_pos, z)
+                fake_u_pos, fake_u_dist, fake_s_pos, fake_s_dist = fake
+                fake_pos, fake_dist, fake_batch = generate_batch(
+                    fake_u_pos, fake_u_dist, fake_s_pos, fake_s_dist)
+                out_fake = D(fake_pos, fake_dist, fake_batch)
+                loss = -out_fake.mean()
+                loss.backward()
+                G_optimizer.step()
 
-    print('Epoch {} done!'.format(epoch))
-    print('Loss: {:.4f}'.format(total_loss / len(loader)))
-    torch.save(G.state_dict(), 'G_ref.pt')
-    torch.save(D.state_dict(), 'D_ref.pt')
+            total_loss += D_loss.abs().item()
+
+        print('Num points: {}, Epoch: {:03d}, Loss: {:.6f}'.format(
+            num_points, epoch, total_loss / len(loader)))
+
+        torch.save(G.state_dict(), osp.join(args.category, 'Gref.pt'))
+        torch.save(D.state_dict(), osp.join(args.category, 'Dref.pt'))
+
+        if epoch % 100 == 0:
+            name = 'Gref_{}_{}.pt'.format(num_points, epoch)
+            path = torch.save(G.state_dict(), osp.join(args.category, name))
+            name = 'Dref_{}_{}.pt'.format(num_points, epoch)
+            path = torch.save(D.state_dict(), osp.join(args.category, name))
