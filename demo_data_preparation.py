d832cfe88a3cef90fb27241b6891c0a5d5066344
Marian <mail@marian42.de>
2020-01-20 22:05:38 +0100

    Update the demo_data_preparation.py script so that it uses the mesh_to_sdf module

--- demo_data_preparation.py
+++ demo_data_preparation.py
@@ -1,8 +1,9 @@
-from sdf.mesh_to_sdf import MeshSDF, scale_to_unit_sphere
-from sdf.pyrender_wrapper import render_normal_and_depth_buffers
-from sdf.scan import get_camera_transform
+from mesh_to_sdf import scale_to_unit_sphere, get_surface_point_cloud
+from mesh_to_sdf.pyrender_wrapper import render_normal_and_depth_buffers
+from mesh_to_sdf.scan import get_camera_transform
 import pyrender
 import trimesh
+import skimage
 import numpy as np
 import math
 from matplotlib import pyplot as plt
@@ -26,7 +27,7 @@ scene.add(pyrender.Mesh.from_trimesh(mesh, smooth=False))
 print("Now showing the input model as a triangle mesh.\nClose the window to continue.")
 pyrender.Viewer(scene, use_raymond_lighting=True)
 
-camera_transform = get_camera_transform(-140, -20)
+camera_transform = get_camera_transform(math.radians(-140), math.radians(-20))
 camera = pyrender.PerspectiveCamera(yfov=2 * math.asin(1.0 / 2) * 0.97, aspectRatio=1.0, znear = 2 - 1.0, zfar = 2 + 1.0)
 normal_buffer, depth_buffer = render_normal_and_depth_buffers(mesh, camera, camera_transform, 1080)
 print("Now showing the normal map of a render of the mesh.\nClose the window to continue.")
@@ -35,10 +36,10 @@ show_image(normal_buffer)
 print("Now showing the depth map of a render of the mesh.\nClose the window to continue.")
 show_image(depth_buffer, grayscale=True)
 
-mesh_sdf = MeshSDF(mesh)
+surface_point_cloud = get_surface_point_cloud(mesh)
 
 print("Now showing the surface point cloud with normals.\nClose the window to continue.")
-mesh_sdf.show_pointcloud()
+surface_point_cloud.show()
 
 print('Calculating...')
 resolution = 800
@@ -50,7 +51,7 @@ points = np.meshgrid(
 )
 
 points = np.stack(points).reshape(3, -1).transpose()
-sdf = mesh_sdf.get_sdf_in_batches(points).reshape(1, resolution, resolution)[0, :, :]
+sdf = surface_point_cloud.get_sdf_in_batches(points).reshape(1, resolution, resolution)[0, :, :]
 clip = 0.2
 sdf = np.clip(sdf, -clip, clip) / clip
 
@@ -61,15 +62,20 @@ image[np.abs(sdf) < 0.02, :] = 0
 print("Now showing a slice through the SDF of the model.\nClose the window to continue.")
 show_image(image)
 
+voxels = surface_point_cloud.get_voxels(voxel_resolution=64)
+vertices, faces, normals, _ = skimage.measure.marching_cubes_lewiner(voxels, level=0)
+mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_normals=normals)
 print("Now showing a voxel volume reconstructed with Marching Cubes.\nClose the window to continue.")
-mesh_sdf.show_reconstructed_mesh(voxel_resolution=64)
+mesh.show()
 
-points, sdf = mesh_sdf.get_sample_points()
-scene = pyrender.Scene()
-colors = np.zeros((points.shape[0], 3))
+points, sdf = surface_point_cloud.sample_sdf_near_surface(number_of_points=150000)
+
+colors = np.zeros(points.shape)
 colors[sdf < 0, 2] = 1
 colors[sdf > 0, 0] = 1
 cloud = pyrender.Mesh.from_points(points, colors=colors)
+
+scene = pyrender.Scene()
 scene.add(cloud)
 print("Now showing a point cloud of non-uniformly sampled SDF data. Negative distances are red, positive distances are blue.")
 viewer = pyrender.Viewer(scene, use_raymond_lighting=True, point_size=2)
