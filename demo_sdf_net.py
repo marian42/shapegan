35b85de2660d543ca48b1b8138b7deecde269642
Marian <mail@marian42.de>
2020-01-29 02:24:07 +0100

    Update parameters in the SDF net demo script

--- demo_sdf_net.py
+++ demo_sdf_net.py
@@ -16,11 +16,11 @@ TRANSITION_FRAMES = 60
 ROTATE_MODEL = False
 USE_HYBRID_GAN = True
 
-SURFACE_LEVEL = 0.048 if USE_HYBRID_GAN else 0.011
+SURFACE_LEVEL = 0.04 if USE_HYBRID_GAN else 0.011
 
 sdf_net = SDFNet()
 if USE_HYBRID_GAN:
-    sdf_net.filename = 'hybrid_progressive_gan_generator_2.to'
+    sdf_net.filename = 'hybrid_progressive_gan_generator_3.to'
 sdf_net.load()
 sdf_net.eval()
 
@@ -45,7 +45,7 @@ def create_image_sequence():
             code = torch.tensor(spline(float(sample_index) + step / TRANSITION_FRAMES), dtype=torch.float32, device=device)
             if ROTATE_MODEL:
                 viewer.rotation = (147 + frame_index / (SAMPLE_COUNT * TRANSITION_FRAMES) * 360 * 6, 40)
-            viewer.set_mesh(sdf_net.get_mesh(code, voxel_resolution=64, sphere_only=False, level=SURFACE_LEVEL))
+            viewer.set_mesh(sdf_net.get_mesh(code, voxel_resolution=128, sphere_only=False, level=SURFACE_LEVEL))
             image = viewer.get_image(flip_red_blue=True)
             cv2.imwrite("images/frame-{:05d}.png".format(frame_index), image)
             frame_index += 1
@@ -77,19 +77,22 @@ def show_models():
 def create_objects():
     from util import ensure_directory
     from rendering.raymarching import render_image
+    from rendering.math import get_rotation_matrix
     import os
     ensure_directory('generated_objects/')
-    image_filename = 'generated_objects/{:05d}.png'
-    mesh_filename = 'generated_objects/{:05d}.stl'
+    image_filename = 'generated_objects/chair-{:03d}.png'
+    mesh_filename = 'generated_objects/chair-{:03d}.stl'
     index = 0
     while True:
         if os.path.exists(image_filename.format(index)) or os.path.exists(mesh_filename.format(index)):
             index += 1
             continue
         latent_code = standard_normal_distribution.sample((LATENT_CODE_SIZE,)).to(device)
-        image = render_image(sdf_net, latent_code, resolution=800, sdf_offset=-SURFACE_LEVEL, ssaa=2, radius=1.4, color=(0.7, 0.7, 0.7))
+        image = render_image(sdf_net, latent_code, resolution=128, sdf_offset=-SURFACE_LEVEL, ssaa=2, radius=1.4, color=(0.7, 0.7, 0.7))
         image.save(image_filename.format(index))
-        mesh = sdf_net.get_mesh(latent_code, voxel_resolution=128, sphere_only=False, level=SURFACE_LEVEL)
+        mesh = sdf_net.get_mesh(latent_code, voxel_resolution=256, sphere_only=False, level=SURFACE_LEVEL)
+        mesh.apply_transform(get_rotation_matrix(90, 'x'))
+        mesh.apply_translation((0, 0, -np.min(mesh.vertices[:, 2])))
         mesh.export(mesh_filename.format(index))
         print("Created mesh for index {:d}".format(index))
         index += 1
